2cca54394f3ff662f16b1b937531f50c3429b63a
HendMegens <hendrik-jan.megens@regionaalenergieloket.nl>
2023-12-04 11:19:24 +0100

    Trying to fix table errors

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
-const htmlParser = require('htmlparser');
-const R = require('ramda');
-const { paragraph, styles } = require('./helpers');
+const htmlParser = require("htmlparser");
+const R = require("ramda");
+const { paragraph, styles } = require("./helpers");
 const htmlAttrs = {
     tag: {
-        ul: 'unordered-list',
-        ol: 'ordered-list',
-        li: 'list-item',
-        blockquote: 'blockquote',
-        p: 'paragraph',
-        h1: 'heading-1',
-        h2: 'heading-2',
-        h3: 'heading-3',
-        h4: 'heading-4',
-        h5: 'heading-5',
-        h6: 'heading-6',
-        hr: 'hr',
-        br: 'br',
-        a: 'hyperlink',
-        b: 'bold',
-        strong: 'bold',
-        code: 'text',
-        i: 'italic',
-        em: 'italic',
-        u: 'underline',
-        img: 'embedded-asset-block',
-        span: 'text',
-        table: 'table',
-        th: 'table-header-cell',
-        tr: 'table-row',
-        td: 'table-cell'
+        ul: "unordered-list",
+        ol: "ordered-list",
+        li: "list-item",
+        blockquote: "blockquote",
+        p: "paragraph",
+        h1: "heading-1",
+        h2: "heading-2",
+        h3: "heading-3",
+        h4: "heading-4",
+        h5: "heading-5",
+        h6: "heading-6",
+        hr: "hr",
+        br: "br",
+        a: "hyperlink",
+        b: "bold",
+        strong: "bold",
+        code: "text",
+        i: "italic",
+        em: "italic",
+        u: "underline",
+        img: "embedded-asset-block",
+        span: "text",
+        table: "table",
+        th: "table-header-cell",
+        tr: "table-row",
+        td: "table-cell",
     },
-    text: 'text',
+    text: "text",
 };
 
 let transformed = []; //What should come out in the end
@@ -39,11 +39,10 @@ const transformDom = (dom) => {
     let results = [];
 
     R.forEach((elm) => {
-
         //RTF Tables in Contentful. Contentful currently pass a 'tbody' tag which isn't supported when importing. This removes it from the hierarchy.
-        if (elm?.children?.[0].name == 'tbody') {
-            elm.children.splice(0, 1, ...elm.children[0].children);
-        }
+        // if (elm?.children?.[0].name == 'tbody') {
+        // elm.children.splice(0, 1, ...elm.children[0].children);
+        // }
 
         const { type, name, data, attribs, children } = elm;
         //console.log(elm);
@@ -53,46 +52,46 @@ const transformDom = (dom) => {
             content = transformDom(children);
         }
 
-        if (type === 'text') {
+        if (type === "text") {
             newData = {
                 data: {},
                 marks: [],
                 value: data,
                 nodeType: type,
             };
-        } else if (type === 'tag') {
-            switch(name) {
-                case 'span':
+        } else if (type === "tag") {
+            switch (name) {
+                case "span":
                     //Spans seem to just be passed through
                     newData = content;
                     break;
-                case 'code':
-                    const Entities = require('html-entities').XmlEntities;
+                case "code":
+                    const Entities = require("html-entities").XmlEntities;
                     const entities = new Entities();
 
                     newData = R.map((node) => {
-                        node = R.assoc('value', entities.decode(node.value), node);
-                        node = R.assoc('marks', R.append({type: 'code'}, node.marks), node);
+                        node = R.assoc("value", entities.decode(node.value), node);
+                        node = R.assoc("marks", R.append({ type: "code" }, node.marks), node);
                         return node;
                     }, content);
                     break;
-                case 'img':
-                    const fileName = R.last(R.split('/', attribs.src));
+                case "img":
+                    const fileName = R.last(R.split("/", attribs.src));
 
                     newData = {
                         data: {
                             target: {
                                 sys: {
                                     space: {},
-                                    type: 'Asset',
-                                    createdAt: '',
-                                    updatedAt: '',
+                                    type: "Asset",
+                                    createdAt: "",
+                                    updatedAt: "",
                                     environment: {},
                                     revision: null,
-                                    locale: 'en-US',
+                                    locale: "en-US",
                                 },
                                 fields: {
-                                    title: R.head(R.split('.', fileName)),
+                                    title: R.head(R.split(".", fileName)),
                                     description: attribs.alt,
                                     file: {
                                         url: attribs.src,
@@ -104,7 +103,7 @@ const transformDom = (dom) => {
                                             },
                                         },
                                         fileName,
-                                        contentType: 'image/' + R.last(R.split('.', fileName)),
+                                        contentType: "image/" + R.last(R.split(".", fileName)),
                                     },
                                 },
                             },
@@ -112,31 +111,31 @@ const transformDom = (dom) => {
                         content: [],
                         nodeType: htmlAttrs[type][name],
                     };
-                    break
-                case 'i':
-                case 'b':
-                case 'strong':
-                case 'u':
+                    break;
+                case "i":
+                case "b":
+                case "strong":
+                case "u":
                     newData = styles(content, htmlAttrs[type][name]);
                     break;
-                case 'a':
+                case "a":
                     newData = {
-                        data: { uri: R.propOr('', 'href', attribs) },
+                        data: { uri: R.propOr("", "href", attribs) },
                         content,
                         nodeType: htmlAttrs[type][name],
                     };
                     break;
-                case 'li':
+                case "li":
                     //@TODO shouldn't need to cast to an array...
-                    content = R.type(content) === 'Array' ? content : [content];
+                    content = R.type(content) === "Array" ? content : [content];
                     let newContent = [];
 
                     //Seems to want text wrapped in some type of content tag (p, h*, etc)
-                    content = R.forEach((node)=> {
-                        if (node.nodeType === 'text' || node.nodeType === 'hyperlink') {
+                    content = R.forEach((node) => {
+                        if (node.nodeType === "text" || node.nodeType === "hyperlink") {
                             //if the last of new content isn't a `paragraph`
-                            if (R.propOr(false, 'nodeType', R.last(newContent)) !== 'paragraph') {
-                                newContent = R.concat(newContent, paragraph([], 'paragraph'));
+                            if (R.propOr(false, "nodeType", R.last(newContent)) !== "paragraph") {
+                                newContent = R.concat(newContent, paragraph([], "paragraph"));
                             }
                             //put node in R.last(newContent).content
                             newContent[newContent.length - 1].content.push(node);
@@ -151,18 +150,18 @@ const transformDom = (dom) => {
                         nodeType: htmlAttrs[type][name],
                     };
                     break;
-                case 'p':
-                case 'h1':
-                case 'h2':
-                case 'h3':
-                case 'h4':
-                case 'h5':
-                case 'h6':
-                    newData = paragraph(content, htmlAttrs[type][name])
-                    break
+                case "p":
+                case "h1":
+                case "h2":
+                case "h3":
+                case "h4":
+                case "h5":
+                case "h6":
+                    newData = paragraph(content, htmlAttrs[type][name]);
+                    break;
                 default:
                     if (!htmlAttrs[type][name]) {
-                        console.log('*** new data needed under -', type, name);
+                        console.log("*** new data needed under -", type, name);
                     }
 
                     newData = {
@@ -173,11 +172,10 @@ const transformDom = (dom) => {
                     break;
             }
         } else {
-            console.log('***new type needed -', type, data);
+            console.log("***new type needed -", type, data);
         }
 
-
-        results = R.type(newData) === 'Array' ? R.concat(results, newData) : R.append(newData, results);
+        results = R.type(newData) === "Array" ? R.concat(results, newData) : R.append(newData, results);
     }, dom);
     return results;
 };
@@ -189,7 +187,7 @@ const handleFn = (error, dom) => {
     transformed = {
         data: {},
         content: transformDom(dom),
-        nodeType: 'document',
+        nodeType: "document",
     };
 };
 
